5017837a91f606d4f35d8f263d18f9ce5080dc69
Hackbright Computer <none@none.com>
2012-10-25 16:53:17 -0700

    add for realz

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ model.py
 """
 import sqlite3
 
+
 def connect_db():
     return sqlite3.connect("tipsy.db")
 
@@ -16,7 +17,8 @@ def new_user(db, email, password, name):
 
 def make_user(row):
     fields = ["id", "email", "password", "username"]
-    return dict(zip(fields, row))
+    result= dict(zip(fields, row))
+    return result["id"]
 
 def authenticate(db, email, password):
     c = db.cursor()
@@ -40,7 +42,47 @@ def get_user(db, user_id):
 
     return None
 
-def new_task(db, title, user_id = None):
+def get_task(db, task_id):
+    """Gets a single task, given its id. Returns a dictionary of the task data."""
+    c = db.cursor()
+    query = """SELECT * from Tasks WHERE id = ?"""
+    c.execute(query, (task_id,))
+    task_row = c.fetchone()
+    if task_row:
+        return make_task(task_row)
+
+    return None
+
+def get_tasks(db, user_id=None):
+    """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
+    c = db.cursor()
+    if user_id:
+        query = """SELECT * from Tasks WHERE user_id = ?"""
+        c.execute(query, (user_id,))
+    else:
+        query = """SELECT * from Tasks"""
+        c.execute(query)
+    tasks = []
+    rows = c.fetchall()
+    for row in rows:
+        task = make_task(row)
+        tasks.append(task)
+    return tasks
+
+def get_from_table_by_id(db, table_name, id):
+    c = db.cursor()
+    query_template = """SELECT * from %s WHERE id=?"""
+    query = query_template%table_name
+    c.execute(query, (id,))
+    row=c.fetchone()
+    if row:
+        if table_name == "Users":
+            return make_user(row)
+        elif table_name=="Tasks":
+            return make_task(row)
+    return None 
+
+def new_task(db, title, user_id):
     """Given a title and a user_id, create a new task belonging to that user. Return the id of the created task"""
     c = db.cursor()
     query = """INSERT into Tasks values (null, ?, DATETIME('now'), null, ?)"""
@@ -60,33 +102,6 @@ def complete_task(db, task_id):
     else:
         return None
 
-def get_tasks(db, user_id=None):
-    """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
-    c = db.cursor()
-    if user_id:
-        query = """SELECT * from Tasks WHERE user_id = ?"""
-        c.execute(query, (user_id,))
-    else:
-        query = """SELECT * from Tasks"""
-        c.execute(query)
-    tasks = []
-    rows = c.fetchall()
-    for row in rows:
-        task = make_task(row)
-        tasks.append(task)
-
-    return tasks
-
-def get_task(db, task_id):
-    """Gets a single task, given its id. Returns a dictionary of the task data."""
-    c = db.cursor()
-    query = """SELECT * from Tasks WHERE id = ?"""
-    c.execute(query, (task_id,))
-    task_row = c.fetchone()
-    if task_row:
-        return make_task(task_row)
-
-    return None
 
 def make_task(row):
     columns = ["id", "title", "created_at", "completed_at", "user_id"]
--- tipsy.py
+++ tipsy.py
@@ -1,39 +1,102 @@
 """
 tipsy.py -- A flask-based todo list
 """
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, g
 import model
 
+
+
 app = Flask(__name__)
+
+#configuration
+app.secret_key = 'admin'
+
+@app.before_request
+def set_up_db():
+	g.db = model.connect_db()
 
 @app.route("/")
 def index():
     return render_template("index.html", user_name="chriszf")
 
+@app.route("/login")
+def login():
+	error="no error"
+	return render_template("login.html" , error=error)
+
+@app.route("/logout")
+def logout():
+	session.pop('user_id', None)
+	print session
+	return redirect("/")
+
+@app.route("/authenticate", methods=["POST"])
+def authenticate():
+	email=request.form['email']
+	password=request.form['password']
+	user_id=model.authenticate(g.db, email, password)
+	if user_id == None:
+		error="no user with that combination"
+		return render_template("/login.html", error=error)
+	session['user_id']=user_id
+	return redirect("/tasks")
+
+@app.route("/new_user")
+def new_user():
+ 	return render_template("new_user.html")
+
+
+@app.route("/save_user", methods=["POST"])
+def save_user():
+	email = request.form['email']
+	password = request.form['password']
+	name = request.form['name']
+	user_id=model.new_user(g.db, email, password, name)
+	session['user_id']=user_id
+	return redirect("/tasks")
+
 @app.route("/save_task", methods=["POST"])
 def save_task():
-    db = model.connect_db()
     title = request.form['title']
-    model.new_task(db, title)
+    user_id=session['user_id']
+    model.new_task(g.db, title,user_id)
     return redirect("/tasks")
 
 @app.route("/tasks")
 def list_tasks():
-    db = model.connect_db()
-    tasks_from_db = model.get_tasks(db, None)
-    return render_template("list_tasks.html", tasks=tasks_from_db)
+	print session
+	user_id = session.get('user_id', None)
+	tasks_from_db = model.get_tasks(g.db, user_id)
+	return render_template("list_tasks.html", tasks=tasks_from_db)
+
+@app.route("/completed_task/<int:task_id>", methods=["POST"])
+def completed_task(task_id):
+	model.complete_task(g.db, task_id)
+	return redirect("/tasks")
+
 
 @app.route("/task/<int:id>", methods=["GET"])
 def view_task(id):
-    db = model.connect_db()
-    task_from_db = model.get_task(db, id)
+    task_from_db = model.get_task(g.db, id)
     return render_template("view_task.html", task=task_from_db)
 
 @app.route("/task/<int:id>", methods=["POST"])
 def complete_task(id):
-    db = model.connect_db()
-    model.complete_task(db, id)
+    model.complete_task(g.db, id)
     return redirect("/tasks")
+
+@app.route("/set_date")
+def set_date():
+	session['date'] = datetime.datetime.now()
+	return "Date set"
+
+@app.route("/get_date")
+def get_date():
+	return str(session['date'])
+
+@app.teardown_request
+def disconnect_db(exception):
+	g.db.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
